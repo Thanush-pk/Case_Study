484c7b043d9d6fefa1bd0d50a9975be7a9cedcfd
thanush <thanush.pk@sonata-software.com>
2022-09-30 10:56:30 +0530

    file update

--- ProductDetails/src/main/java/com/sonata/mvc/controller/ProductController.java
+++ ProductDetails/src/main/java/com/sonata/mvc/controller/ProductController.java
@@ -14,11 +14,12 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.sonata.mvc.model.Products;
 import com.sonata.mvc.repository.ProductRepository;
 
-
+@CrossOrigin(origins="http://localhost:4200")
 @RestController
 public class ProductController {
 	
